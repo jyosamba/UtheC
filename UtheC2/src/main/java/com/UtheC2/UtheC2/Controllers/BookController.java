3ba44f482000f8860e510906e96d747c37dbad93
name <email@email.com>
2023-10-26 16:22:26 +0100

    file path thymeleafe

--- UtheC2/src/main/java/com/UtheC2/UtheC2/Controllers/BookController.java
+++ UtheC2/src/main/java/com/UtheC2/UtheC2/Controllers/BookController.java
@@ -5,6 +5,7 @@ import com.UtheC2.UtheC2.Services.BookServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/books")
 public class BookController {
     @Autowired
@@ -39,6 +40,7 @@ public class BookController {
 
         return ResponseEntity.ok(booksList);
     }
+
 
 
 }
