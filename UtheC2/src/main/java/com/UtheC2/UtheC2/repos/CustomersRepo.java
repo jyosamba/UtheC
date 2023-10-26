7d532b6edc2841f0373248a94f5229bfb5806451
ammarah23 <u1964819@unimail.hud.ac.uk>
2023-10-26 15:02:15 +0100

    CustomerController

--- UtheC2/src/main/java/com/UtheC2/UtheC2/repos/CustomersRepo.java
+++ UtheC2/src/main/java/com/UtheC2/UtheC2/repos/CustomersRepo.java
@@ -3,5 +3,8 @@ package com.UtheC2.UtheC2.repos;
 import com.UtheC2.UtheC2.Entities.CustomersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomersRepo extends JpaRepository<CustomersEntity,Integer> {
+    List<CustomersEntity> findByCustomersCustomersID(int customerID);
 }
