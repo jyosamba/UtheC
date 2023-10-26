aed2a7e7dd5b033c4cb4debd6e3b926841c14918
name <email@email.com>
2023-10-26 12:39:14 +0100

    UserController skeleton

---
+++ UtheC2/src/main/java/com/UtheC2/UtheC2/Controllers/UserController.java
@@ -0,0 +1,42 @@
+package com.UtheC2.UtheC2.Controllers;
+
+import com.UtheC2.UtheC2.Entities.PublishersEntity;
+import com.UtheC2.UtheC2.repos.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    AuthorRepo Repo;
+
+    @Autowired
+    BooksRepo RepoB;
+
+    @Autowired
+    CustomersRepo RepoC;
+
+    @Autowired
+    GenresRepo RepoG;
+
+    @Autowired
+    OrderDetailsRepo RepoOD;
+
+    @Autowired
+    OrdersRepo RepoO;
+
+    @Autowired
+    PaymentsRepo RepoPay;
+
+    @Autowired
+    PublishersRepo RepoP;
+
+    @Autowired
+    UserRepo RepoU;
+
+    @GetMapping("/user")
+    public String method(){ return "";}
+
+}
